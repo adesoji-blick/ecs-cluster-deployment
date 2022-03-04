4d5e6d67668f493676f7064ea2931ec275726676
Adesoji A <aiyemonase@Adesojis-MacBook-Pro.local>
2022-03-04 10:40:15 -0700

    updated.sh file

--- updated.sh
+++ updated.sh
@@ -8,58 +8,33 @@ CLUSTER=direction-app-cluster
 FAMILY=direction-task-definition
 NAME=direction-task-definition
 SERVICE_NAME=direction-app-service
-BUILD_NUMBER=301
+# BUILD_NUMBER=401
 env
 aws configure list
 echo $HOME
 
 #Store the repositoryUri as a variable
-# REPOSITORY_URI=`aws ecr describe-repositories --repository-names direction-app --region ca-central-1 | jq '.repositories[] |.repositoryUri'`
-IMAGE_URI=319670758662.dkr.ecr.ca-central-1.amazonaws.com/direction-app:0.0.${BUILD_NUMBER}
+REPOSITORY_URI=`aws ecr describe-repositories --repository-names direction-app --region ca-central-1 | jq -r '.repositories[] |.repositoryUri'`
+# IMAGE_URI=319670758662.dkr.ecr.ca-central-1.amazonaws.com/direction-app:0.0.${BUILD_NUMBER}
+IMAGE_URI=`$REPOSITORY_URI:0.0.${BUILD_NUMBER}`
 
 # Fetch task definition to be updated and store as contants
-TASK_DEFINITION=`aws ecs describe-task-definition --task-definition direction-task-definition --region ca-central-1`
+TASK_DEFINITION=`aws ecs describe-task-definition --task-definition ${FAMILY} --region ${REGION}`
 
 # update task definition with new image URL
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$IMAGE_URI" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) | del(.registeredAt) | del(.registeredBy)')
 
 # register new task definition
-aws ecs register-task-definition --region "ca-central-1" --cli-input-json "$NEW_TASK_DEFINTIION"
+aws ecs register-task-definition --region ${REGION} --cli-input-json "$NEW_TASK_DEFINTIION"
 
-# set revision contannt
-REVISION=`aws ecs describe-task-definition --task-definition direction-task-definition --region ca-central-1 | jq .taskDefinition.revision`
-
-# update service
-# aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
-
-SERVICE=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq '.services[] |.failures'`
-
-# ROLLOUT_STATE=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq '.services []|.deployments[]|.rolloutState'`
-#   if [ ${ROLLOUT_STATE} = "COMPLETED"]; then
+#set revision contantS
+REVISION=`aws ecs describe-task-definition --task-definition ${FAMILY} --region ${REGION} | jq .taskDefinition.revision`
+#Set services constant
+SERVICE=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq '.services[] |.status'`
 
 # stop ecs service - task
 #Create or update service
-if [ "$SERVICE" == "" ]; then
-  echo "entered existing service"
-  DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq '.services[] |.desiredCount'`
-  RUNNING_TASK=`aws ecs list-tasks --cluster ${CLUSTER} --region ${REGION} | jq -r '.taskArns[0]'`
-  echo $RUNNING_TASK
-  echo "running task is"
-  if [ "$RUNNING_TASK" !=  "null" ];  then
-    aws ecs stop-task --cluster ${CLUSTER} --region ${REGION} --task $RUNNING_TASK
-  fi
-  if [ ${DESIRED_COUNT} = "0" ]; then
-    DESIRED_COUNT="1"
-  fi
-  aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
-else
-  echo "entered new service"
-  aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER} --region ${REGION}
-fi
-
-#Stop ecs service - task
-#Create or update service
-if [ "$SERVICE" == "" ]; then  
+if [ "$SERVICE" != "null" ]; then  
   echo "entered existing service"
   DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq '.services[] |.desiredCount'`
   RUNNING_TASK=`aws ecs list-tasks --cluster ${CLUSTER} --region ${REGION} | jq -r '.taskArns[0]'`
