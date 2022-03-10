6e0609e69ea0b92782f6ca330f9720946391f205
Adesoji A <aiyemonase@Adesojis-MacBook-Pro.local>
2022-03-09 18:11:17 -0700

    updated jenkinsfile

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ CLUSTER=direction-app-cluster
 FAMILY=direction-task-definition
 NAME=direction-task-definition
 SERVICE_NAME=direction-app-service
-BUILD_NUMBER=201
+BUILD_NUMBER=301
 env
 aws configure list
 echo $HOME
